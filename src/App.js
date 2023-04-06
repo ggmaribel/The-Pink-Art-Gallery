26207b6534a2bdfe24f04ba55f0430f3a9b45c38
ggmaribel <ggmaribelg@gmail.com>
2023-04-05 21:15:32 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {  Header, Footer } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <Footer />
+  </div>
+);
+
+export default App;
