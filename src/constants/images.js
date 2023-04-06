c6fc71576a0a746ae68a53e1c8dc96e8dc684513
ggmaribel <ggmaribelg@gmail.com>
2023-04-06 16:17:11 -0600

    added carousel

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,4 +1,4 @@
-import logo from '../assets/logo.png';
+
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
 import gallery03 from '../assets/gallery03.png';
@@ -11,9 +11,23 @@ import gallery09 from '../assets/gallery09.png';
 import gallery10 from '../assets/gallery10.png';
 import gallery11 from '../assets/gallery11.png';
 import gallery12 from '../assets/gallery12.png';
+import gallery13 from '../assets/gallery13.png';
+import gallery14 from '../assets/gallery14.png';
+import gallery15 from '../assets/gallery15.png';
+import gallery16 from '../assets/gallery16.png';
+import gallery17 from '../assets/gallery17.png';
+import gallery18 from '../assets/gallery18.png';
+import gallery19 from '../assets/gallery19.png';
+import gallery20 from '../assets/gallery20.png';
+import gallery21 from '../assets/gallery21.png';
+import gallery22 from '../assets/gallery22.png';
+import gallery23 from '../assets/gallery23.png';
+import gallery24 from '../assets/gallery24.png';
+import gallery25 from '../assets/gallery25.png';
+import gallery26 from '../assets/gallery26.png';
+import gallery27 from '../assets/gallery27.png';
 
 export default {
-    logo,
     gallery01,
     gallery02,
     gallery03,
@@ -26,4 +40,19 @@ export default {
     gallery10,
     gallery11,
     gallery12,
+    gallery13,
+    gallery14,
+    gallery15,
+    gallery16,
+    gallery17,
+    gallery18,
+    gallery19,
+    gallery20,
+    gallery21,
+    gallery22,
+    gallery23,
+    gallery24,
+    gallery25,
+    gallery26,
+    gallery27,
   };
